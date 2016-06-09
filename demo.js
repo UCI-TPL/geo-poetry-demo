9e870a8c1173622429b29caca1fb88062ebcea27
Jordan Rickman <jrickman@uci.edu>
2016-06-09 14:25:23 -0700

    Now querying the server for the list of genres.

--- demo.js
+++ demo.js
@@ -1,7 +1,25 @@
+var DEFAULT_ENERGY = 0.5;
+var SERVER_BASE_URL = "http://127.0.0.1:5000";
+
 var app = angular.module('GeoPoetryApp', []);
 
-app.controller('GeoPoetryController', function GeoPoetryController($scope) {
-	$scope.genres = ["Jazz", "Rock"];
+app.controller('GeoPoetryController', ['$scope', '$http', function GeoPoetryController($scope, $http) {
+	$scope.genres = [];
+	$scope.selected_genre = "-";
 	$scope.energy = 0.5;
 	$scope.poetry_lines = ["Here is some poetry.", "I hope you like it."];
-});
+
+	$http.get(SERVER_BASE_URL+"/get-genres").then(
+		function successCallback(response) {
+			$scope.genres = response.data['genres'];
+		},
+		function errorCallback(response) {
+			// TODO
+			console.log(response);
+		}
+	)
+
+	$scope.submitForm = function() {
+		// TODO
+	};
+}]);
