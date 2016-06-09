e62f148057bf5a4761deadfb583f5e1f91169aeb
Jordan Rickman <jrickman@uci.edu>
2016-06-09 14:38:03 -0700

    Better number validation via the directive at https://github.com/FCSAmerica/angular-fcsa-number

--- demo.js
+++ demo.js
@@ -1,7 +1,7 @@
 var DEFAULT_ENERGY = 0.5;
 var SERVER_BASE_URL = "http://127.0.0.1:5000";
 
-var app = angular.module('GeoPoetryApp', []);
+var app = angular.module('GeoPoetryApp', ['fcsa-number']);
 
 app.controller('GeoPoetryController', ['$scope', '$http', function GeoPoetryController($scope, $http) {
 	$scope.genres = [];
